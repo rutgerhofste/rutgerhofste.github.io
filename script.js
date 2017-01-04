aaf69c02f70abe8e6c6d2d1368ace0de26823038
rutgerhofste <rutgerhofste@gmail.com>
2017-01-03 19:39:25 -0500

    add padding var

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    padding = 100 ;
 
 
 
