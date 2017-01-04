1f7ef5b6475fd3b351faea2120c25394d57c4c23
Rutger Hofste <rutgerhofste@gmail.com>
2017-01-04 16:03:46 -0500

    last version before new template

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ var valueline = d3.line()
     .x(function(d) { return x(d.xvalue); })
     .y(function(d) { return y(d.yvalue); });
 
-var div = d3.select("body").append("div")
+
+var div = d3.select("charts").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
   
 
 // Set the dimensions of the canvas / graph
 var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 
 
@@ -32,50 +33,94 @@ var y = d3.scaleLinear().range([height, 0]);
 x.domain([0,7]);
 y.domain([0,100]);
 
+
+
 // Adds the svg canvas
-var svg = d3.select("body")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .style('background','grey')
-    .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("charts")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style('background','grey')
+    .call(d3.zoom().on("zoom", function () {
+        svg.attr("transform", d3.event.transform)
+    }))
+
+  .append("g")
+      .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the scatterplot
+  svg.selectAll("dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("r", 10)
+      .attr("cx", function(d) { return x(d.xvalue); })
+      .attr("cy", function(d) { return y(d.yvalue); })
+      .on("mouseover", handleMouseOver)
+      .on("mouseout",handleMouseOut)
+      .on("click",handleMouseClick);
+
+var charts = d3.select("charts");
 
 
-    // Add the x Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+charts.call(d3.zoom()
+    .scaleExtent([1 / 2, 4])
+    .on("zoom", zoomed));  
 
-    // Add the y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+// interactivity scatterplot
 
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
+function handleMouseOver(d){
+  console.log("handleMouseOverTooltip");
+  // tooltip  
+  div.transition()
+    .duration(500)
+    .style("opacity", .5);
+  div.html(d.yvalue)
+    .style("left", (d3.event.pageX) + "px")
+    .style("top", (d3.event.pageY - 28) + "px");
+  // circle
+  d3.select(this).transition()
+    .duration(500)
+    .style("fill","red");
+    
+};
 
-    // Add the scatterplot
-    svg.selectAll("dot")
-        .data(data)
-      .enter().append("circle")
-        .attr("r", 5)
-        .attr("cx", function(d) { return x(d.xvalue); })
-        .attr("cy", function(d) { return y(d.yvalue); })
-        .on("mouseover", function(d) {
-          div.transition()
-            .duration(200)
-            .style("opacity", .9);
-          div.html(d.yvalue)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-          div.transition()
-           .duration(500)
-           .style("opacity", 0);
-       });
+function handleMouseOut(d){
+  console.log("handleMouseOut")
+  div.transition()
+    .duration(500)
+    .style("opacity", 0);
+  // circle
+  d3.select(this).transition()
+    .duration(500)
+    .style("fill","blue");  
+};         
+
+      
+function handleMouseClick(d){
+  console.log("clicked");
+  //d3.select(this).style("fill","red");
+};
+
+
+function zoomed() {
+  console.log("zoomed")
+}
+
+    
       
