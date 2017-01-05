c5a4ecc53f437feb7a6a01eacdb22d5b19f5bd51
rutgerhofste <rutgerhofste@gmail.com>
2017-01-04 22:19:40 -0500

    lots of updates

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-var data2 = [
-  {xvalue: 1, yvalue: 200},
-  {xvalue: 2, yvalue: 280},
-  {xvalue: 3, yvalue: 15},
-  {xvalue: 4, yvalue: 11},
-  {xvalue: 5, yvalue: 14},
-  {xvalue: 6, yvalue: 23},
-  {xvalue: 7, yvalue: 9}
-];
-
-
 
 var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 110, left: 40},
@@ -37,7 +26,7 @@ var zoom = d3.zoom()
     .on("zoom", zoomed);
 
 var area = d3.area()
-    .curve(d3.curveMonotoneX)
+    .curve(d3.curveLinear)
     .x(function(d) { return x(d.date); })
     .y0(height)
     .y1(function(d) { return y(d.price); });
@@ -67,56 +56,66 @@ d3.csv("800KYV2.csv", type, function(error, data) {
 
   //x.domain(d3.extent(data, function(d) { return d.date; }));
   //y.domain([0, d3.max(data, function(d) { return d.price; })]);
-  x.domain([-800000, 2000]);
-  y.domain([150, 300]);
+  x.domain([-800000, 21000]);
+  y.domain([150, 400]);
 
   x2.domain(x.domain());
   y2.domain(y.domain());
 
+
   focus.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area);
+    .datum(data)
+    .attr("class", "area")
+    .attr("d", area);
 
-  focus.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  focus.append("path")
+    .datum(data)
+    .attr("class","line")
+    .attr("d",line);
 
-  focus.append("g")
-      .attr("class", "axis axis--y")
-      .call(yAxis);
-
-  context.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area2);
-
-  context.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height2 + ")")
-      .call(xAxis2);
-
-  context.append("g")
-      .attr("class", "brush")
-      .call(brush)
-      .call(brush.move, x.range());
-
-  svg.append("rect")
-      .attr("class", "zoom")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .call(zoom);
-
-  // Add the scatterplot
-  svg.selectAll("dot")
+// draw dots
+  focus.selectAll(".dot")
       .data(data)
     .enter().append("circle")
-      .attr("r", 10)
-      .attr("cx", function(d) { return x(d.date); })
-      .attr("cy", 100)
-  ;    
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.date)})
+      .attr("cy", function(d) { return y(d.price)})
+      .style("fill","red");
+
+
+  focus.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  focus.append("g")
+    .attr("class", "axis axis--y")
+    .call(yAxis);
+
+  context.append("path")
+    .datum(data)
+    .attr("class", "area")
+    .attr("d", area2);
+
+  context.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(xAxis2);
+
+  context.append("g")
+    .attr("class", "brush")
+    .call(brush)
+    .call(brush.move, x.range());
+
+  svg.append("rect")
+    .attr("class", "zoom")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .call(zoom)
+
+
 
 });
 
@@ -124,11 +123,15 @@ function brushed() {
   if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
   var s = d3.event.selection || x2.range();
   x.domain(s.map(x2.invert, x2));
+  d3.selectAll(".dot").attr("fill","blue");
   focus.select(".area").attr("d", area);
+  focus.select(".line").attr("d", line); // seld added line of code
+  focus.selectAll(".dot").attr("transform", d3.event.transform);
   focus.select(".axis--x").call(xAxis);
   svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
       .scale(width / (s[1] - s[0]))
       .translate(-s[0], 0));
+   // seld added line of code
 }
 
 function zoomed() {
@@ -136,8 +139,11 @@ function zoomed() {
   var t = d3.event.transform;
   x.domain(t.rescaleX(x2).domain());
   focus.select(".area").attr("d", area);
+  focus.select(".line").attr("d", line); // self added line of code
+  focus.selectAll(".dot").attr("transform", d3.event.transform); // seld added line of code
   focus.select(".axis--x").call(xAxis);
   context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+  
 }
 
 function type(d) {
@@ -146,6 +152,11 @@ function type(d) {
   d.bron = d.bron;
   return d;
 }
+
+
+var line = d3.line()
+  .x(function(d) { return x(d.datum); })
+  .y(function(d) { return y(d.waarde); });
 
 
 
